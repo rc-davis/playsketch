951857b0d3c71b9ff4aa259a19cafda9298491ff
Ryder Ziola <ryder@ryderziola.ca>
2012-07-14 11:20:57 +0800

    Update copyright info

--- PlaySketch/PlaySketch/PSDrawingItem.m
+++ PlaySketch/PlaySketch/PSDrawingItem.m
@@ -1,10 +1,15 @@
-//
-//  PSDrawingItem.m
-//  PlaySketch
-//
-//  Created by Ryder Ziola on 12-07-13.
-//  Copyright (c) 2012 Singapore Management University. All rights reserved.
-//
+/*
+ 
+ --------------
+ Copyright 2012 Singapore Management University
+ 
+ This Source Code Form is subject to the terms of the
+ Mozilla Public License, v. 2.0. If a copy of the MPL was
+ not distributed with this file, You can obtain one at
+ http://mozilla.org/MPL/2.0/.
+ --------------
+ 
+ */
 
 #import "PSDrawingItem.h"
 #import "PSDrawingGroup.h"
