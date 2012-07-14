d32a360b028c65700f716a18334f0e922ab9d747
Ryder Ziola <ryder@ryderziola.ca>
2012-07-14 13:54:26 +0800

    Create PSAnimationRenderingView for getting drawing strokes

---
+++ PlaySketch/PlaySketch/PSAnimationRenderingView.m
@@ -0,0 +1,37 @@
+/*
+ 
+ --------------
+ Copyright 2012 Singapore Management University
+ 
+ This Source Code Form is subject to the terms of the
+ Mozilla Public License, v. 2.0. If a copy of the MPL was
+ not distributed with this file, You can obtain one at
+ http://mozilla.org/MPL/2.0/.
+ --------------
+ 
+ */
+
+
+#import "PSAnimationRenderingView.h"
+
+@implementation PSAnimationRenderingView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
