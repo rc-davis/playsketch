de76003209d6f73ee90160206613d2f33b709228
Ryder Ziola <ryder@ryderziola.ca>
2012-07-15 14:38:45 +0800

    Create PSDocumentListController for showing us the documents we have

---
+++ PlaySketch/PlaySketch/PSDocumentListController.m
@@ -0,0 +1,49 @@
+/*
+ 
+ --------------
+ Copyright 2012 Singapore Management University
+ 
+ This Source Code Form is subject to the terms of the
+ Mozilla Public License, v. 2.0. If a copy of the MPL was
+ not distributed with this file, You can obtain one at
+ http://mozilla.org/MPL/2.0/.
+ --------------
+ 
+ */
+
+#import "PSDocumentListController.h"
+
+@interface PSDocumentListController ()
+
+@end
+
+@implementation PSDocumentListController
+@synthesize scrollView = _scrollView;
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+	// Generate a list of documents
+	
+	
+	// make a view for each one and add to the scroll view
+	
+}
+
+- (void)viewDidUnload
+{
+	[super viewDidUnload];
+
+	
+	// Release all of our views
+	
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+@end
