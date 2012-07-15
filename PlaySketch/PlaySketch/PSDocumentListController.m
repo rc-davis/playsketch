0c6cd4a26450541e29bb565b982480af95de21a6
Ryder Ziola <ryder@ryderziola.ca>
2012-07-15 15:27:20 +0800

    Create buttons for each document we fetch

--- PlaySketch/PlaySketch/PSDocumentListController.m
+++ PlaySketch/PlaySketch/PSDocumentListController.m
@@ -22,25 +22,51 @@
 @synthesize scrollView = _scrollView;
 
 
+/*
+	This is called after everything in the storyboard is loaded, but right
+	before the view is shown on the screen.
+	This is our chance to refresh the view's state before presenting it
+*/
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 
-	// Generate a list of documents
+	// Fetch a list of all the documents
 	NSArray* allDocuments = [PSDataModel allDrawingDocumentRoots];
 	
+	// create a view for each document and add to the scroll view
+	CGRect buttonFrame = CGRectMake(0, 0, 200, 400);
+	CGFloat STEPSIZE = buttonFrame.size.width + 200;
+	CGFloat centerX =  STEPSIZE/2;
 	
-	// make a view for each one and add to the scroll view
+	NSLog(@"SCROLLVIEW HEIGHT: %lf", self.scrollView.bounds.size.height);
 	
+	CGSize newContentSize = self.scrollView.contentSize;
+	newContentSize.width = allDocuments.count*STEPSIZE;
+	self.scrollView.contentSize = newContentSize;
+	
+	for(PSDrawingGroup* docRoot in allDocuments)
+	{
+		UIButton* docButton = [[UIButton alloc] initWithFrame:buttonFrame];
+		docButton.backgroundColor = [UIColor colorWithRed:0.627 green:1.000 blue:0.653 alpha:1.000];
+		docButton.center = CGPointMake(centerX, self.scrollView.bounds.size.height/2.0);
+		[self.scrollView addSubview:docButton];
+		centerX += STEPSIZE;
+	}
 }
 
+
+/*
+	This is called right after the view has left the screen. 
+	It gives us the opportunity to free any resources the view was using.
+*/
 - (void)viewDidUnload
 {
 	[super viewDidUnload];
 
 	
 	// Release all of our views
-	
+	// TODO
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
