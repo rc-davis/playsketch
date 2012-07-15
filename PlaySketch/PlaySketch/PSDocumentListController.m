a9ad3141c6ccde8db9797b0765730bd749e413a8
Ryder Ziola <ryder@ryderziola.ca>
2012-07-15 15:41:51 +0800

    Keep references to the buttons we create

--- PlaySketch/PlaySketch/PSDocumentListController.m
+++ PlaySketch/PlaySketch/PSDocumentListController.m
@@ -15,11 +15,17 @@
 #import "PSDataModel.h"
 
 @interface PSDocumentListController ()
+@property(nonatomic,retain)NSArray* documentButtons;
+@property(nonatomic,retain)NSArray* documentRoots;
 
+-(void)generateButtons;
+-(void)clearButtons;
 @end
 
 @implementation PSDocumentListController
 @synthesize scrollView = _scrollView;
+@synthesize documentRoots = _documentRoots;
+@synthesize documentButtons = _documentButtons;
 
 
 /*
@@ -30,29 +36,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-	// Fetch a list of all the documents
-	NSArray* allDocuments = [PSDataModel allDrawingDocumentRoots];
-	
-	// create a view for each document and add to the scroll view
-	CGRect buttonFrame = CGRectMake(0, 0, 200, 400);
-	CGFloat STEPSIZE = buttonFrame.size.width + 200;
-	CGFloat centerX =  STEPSIZE/2;
-	
-	NSLog(@"SCROLLVIEW HEIGHT: %lf", self.scrollView.bounds.size.height);
-	
-	CGSize newContentSize = self.scrollView.contentSize;
-	newContentSize.width = allDocuments.count*STEPSIZE;
-	self.scrollView.contentSize = newContentSize;
-	
-	for(PSDrawingGroup* docRoot in allDocuments)
-	{
-		UIButton* docButton = [[UIButton alloc] initWithFrame:buttonFrame];
-		docButton.backgroundColor = [UIColor colorWithRed:0.627 green:1.000 blue:0.653 alpha:1.000];
-		docButton.center = CGPointMake(centerX, self.scrollView.bounds.size.height/2.0);
-		[self.scrollView addSubview:docButton];
-		centerX += STEPSIZE;
-	}
+	[self generateButtons];
 }
 
 
@@ -63,10 +47,7 @@
 - (void)viewDidUnload
 {
 	[super viewDidUnload];
-
-	
-	// Release all of our views
-	// TODO
+	[self clearButtons];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -75,9 +56,63 @@
 }
 
 
+-(void)generateButtons
+{
+	if(self.documentButtons != nil)
+		[self clearButtons];
+	
+	
+	// Fetch a list of all the documents
+	self.documentRoots = [PSDataModel allDrawingDocumentRoots];
+	NSMutableArray* buttons = [NSMutableArray arrayWithCapacity:self.documentRoots.count];
+	
+	// Set up some size variables for doing the layout of the buttons
+	CGRect buttonFrame = CGRectMake(0, 0, 200, 400);
+	CGFloat STEPSIZE = buttonFrame.size.width + 200;
+	CGFloat centerX =  STEPSIZE/2;	
+
+	// Give the scrollview's scrolling area the right size to hold them all...
+	CGSize newContentSize = self.scrollView.contentSize;
+	newContentSize.width = self.documentRoots.count*STEPSIZE;
+	self.scrollView.contentSize = newContentSize;
+
+	// Create a button for each document and add to the scroll view
+	
+	for(PSDrawingGroup* docRoot in self.documentRoots)
+	{
+		UIButton* docButton = [[UIButton alloc] initWithFrame:buttonFrame];
+		docButton.backgroundColor = [UIColor colorWithRed:0.627 green:1.000 blue:0.653 alpha:1.000];
+		docButton.center = CGPointMake(centerX, self.scrollView.bounds.size.height/2.0);
+		[self.scrollView addSubview:docButton];
+		[buttons addObject:docButton];
+		centerX += STEPSIZE;
+	}
+
+	self.documentButtons = buttons;
+}
+
+
+-(void)clearButtons
+{
+	for(UIButton* button in self.documentButtons)
+	{
+		[button removeFromSuperview];
+	}
+	self.documentButtons = nil;
+	self.documentRoots = nil;
+	self.scrollView.contentSize = self.scrollView.frame.size;
+	
+}
+
+
+
+
 -(IBAction)newDocument:(id)sender
 {
 	PSDrawingGroup* group = [PSDataModel newDocumentRoot];
+	[self generateButtons];
+	
+	//Todo: scroll to center on the new button
 }
 
 @end
