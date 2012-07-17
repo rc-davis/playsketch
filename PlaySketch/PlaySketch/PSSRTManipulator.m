538feec637fc8a6c0539faedd5ffbcfa63ec1426
Ryder Ziola <ryder@ryderziola.ca>
2012-07-17 13:02:49 +0800

    Create a view subclass for the SRT manipulator

---
+++ PlaySketch/PlaySketch/PSSRTManipulator.m
@@ -0,0 +1,29 @@
+/*
+ 
+ --------------
+ Copyright 2012 Singapore Management University
+ 
+ This Source Code Form is subject to the terms of the
+ Mozilla Public License, v. 2.0. If a copy of the MPL was
+ not distributed with this file, You can obtain one at
+ http://mozilla.org/MPL/2.0/.
+ --------------
+ 
+ */
+
+#import "PSSRTManipulator.h"
+
+@implementation PSSRTManipulator
+
+- (id)initWithFrame:(CGRect)frame
+{
+	self = [super initWithFrame:frame];
+	if (self)
+	{
+		self.backgroundColor = [UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:0.5];
+	}
+	return self;
+}
+
+
+@end
