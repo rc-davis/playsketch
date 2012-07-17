d4afa670999da7c85a304dc61b56de32ca0c2fe7
Ryder Ziola <ryder@ryderziola.ca>
2012-07-17 19:24:56 +0800

    Helper for flipping a rect's coordinate system

--- PlaySketch/PlaySketch/PSHelpers.h
+++ PlaySketch/PlaySketch/PSHelpers.h
@@ -11,8 +11,25 @@
  
  */
 
+#ifndef PlaySketch_PSHelpers_h
+#define PlaySketch_PSHelpers_h
+
+
 
 //TODO: conditionally remove this in non-debug builds
 #define PS_ASSERT( expr, message) NSAssert( expr, message);
 #define PS_ASSERT1( expr, message, arg) NSAssert1( expr, message, arg);
 #define PS_NOT_YET_IMPLEMENTED() NSAssert(NO, @"NOT YET IMPLEMENTED" );
+
+
+// Translate a rect from top-0 to bottom-0 coordinate systems (urgh)
+static inline CGRect CGRectByFlippingYOriginDirection(CGRect rect, CGRect parentFrame)
+{
+	return CGRectMake(rect.origin.x,
+					  parentFrame.size.height - (rect.origin.y + rect.size.height),
+					  rect.size.width,
+					  rect.size.height);
+}
+
+
+#endif
