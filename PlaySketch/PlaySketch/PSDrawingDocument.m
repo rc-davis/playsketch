62346d28b3071d72c462d2fa25772abe4c236345
Ryder Ziola <ryder@ryderziola.ca>
2012-07-20 18:11:28 +0800

    Fix up missing copyright headers

--- PlaySketch/PlaySketch/PSDrawingDocument.m
+++ PlaySketch/PlaySketch/PSDrawingDocument.m
@@ -1,10 +1,15 @@
-//
-//  PSDrawingDocument.m
-//  PlaySketch
-//
-//  Created by Ryder Ziola on 12-07-15.
-//  Copyright (c) 2012 Singapore Management University. All rights reserved.
-//
+/*
+ 
+ --------------
+ Copyright 2012 Singapore Management University
+ 
+ This Source Code Form is subject to the terms of the
+ Mozilla Public License, v. 2.0. If a copy of the MPL was
+ not distributed with this file, You can obtain one at
+ http://mozilla.org/MPL/2.0/.
+ --------------
+ 
+ */
 
 #import "PSDrawingDocument.h"
 #import "PSDrawingGroup.h"
--- PlaySketch/PlaySketch/PSSelectionHelper.m
+++ PlaySketch/PlaySketch/PSSelectionHelper.m
@@ -1,10 +1,16 @@
-//
-//  PSSelectionSet.m
-//  PlaySketch
-//
-//  Created by Ryder Ziola on 12-07-16.
-//  Copyright (c) 2012 Singapore Management University. All rights reserved.
-//
+/*
+ 
+ --------------
+ Copyright 2012 Singapore Management University
+ 
+ This Source Code Form is subject to the terms of the
+ Mozilla Public License, v. 2.0. If a copy of the MPL was
+ not distributed with this file, You can obtain one at
+ http://mozilla.org/MPL/2.0/.
+ --------------
+ 
+ */
+
 
 #import "PSSelectionHelper.h"
 
