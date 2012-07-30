4407c33a231c22ad6c8521dd61f39872fc2b831f
Ryder Ziola <ryder@ryderziola.ca>
2012-07-30 09:45:45 +0800

    Removing brush.png resource

---
+++ PlaySketch/PlaySketch/PSGroupOverlayButtons.h
@@ -0,0 +1,13 @@
+//
+//  PSGroupOverlayButtons.h
+//  PlaySketch
+//
+//  Created by Ryder Ziola on 2012-07-30.
+//  Copyright (c) 2012 Singapore Management University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSGroupOverlayButtons : UIView
+
+@end
---
+++ PlaySketch/PlaySketch/PSGroupOverlayButtons.m
@@ -0,0 +1,31 @@
+//
+//  PSGroupOverlayButtons.m
+//  PlaySketch
+//
+//  Created by Ryder Ziola on 2012-07-30.
+//  Copyright (c) 2012 Singapore Management University. All rights reserved.
+//
+
+#import "PSGroupOverlayButtons.h"
+
+@implementation PSGroupOverlayButtons
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
