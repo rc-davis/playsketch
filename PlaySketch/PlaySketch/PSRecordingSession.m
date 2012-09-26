1c47ee4f95717026e992a0fc40cb68c6c053393d
Ryder Ziola <ryder@ryderziola.ca>
2012-09-26 16:06:44 +0800

    Missing copyright notice

--- PlaySketch/PlaySketch/PSRecordingSession.m
+++ PlaySketch/PlaySketch/PSRecordingSession.m
@@ -1,10 +1,16 @@
-//
-//  PSRecordingSession.m
-//  PlaySketch
-//
-//  Created by Ryder Ziola on 2012-09-25.
-//  Copyright (c) 2012 Singapore Management University. All rights reserved.
-//
+/*
+ 
+ --------------
+ Copyright 2012 Singapore Management University
+ 
+ This Source Code Form is subject to the terms of the
+ Mozilla Public License, v. 2.0. If a copy of the MPL was
+ not distributed with this file, You can obtain one at
+ http://mozilla.org/MPL/2.0/.
+ --------------
+ 
+ */
+
 
 #import "PSRecordingSession.h"
 #import "PSDrawingGroup.h"
